cf379e13fa192ab00d4768e7fe6259d8d6ef7e70
Simon Holesch <simon@holesch.de>
2022-09-10 19:54:35 +0200

    Get CONNECT request to work
    Probably not as intended by aiohttp, but works for now.

--- not_my_board/cli/__init__.py
+++ not_my_board/cli/__init__.py
@@ -1,11 +1,12 @@
 import argparse
 import aiohttp.web
 import functools
+import asyncio
 
 from ..__about__ import __version__
 
 
-ROUTES = aiohttp.web.RouteTableDef()
+routes = aiohttp.web.RouteTableDef()
 
 
 def main():
@@ -27,14 +28,86 @@ def main():
 
 
 def export_command(args):
-    app = aiohttp.web.Application()
-    app.add_routes(ROUTES)
+    app = aiohttp.web.Application(middlewares=[middleware_debug])
+    app["config"] = parse_config()
+    app.add_routes(routes)
     aiohttp.web.run_app(app, host='localhost', port=2092)
+
+
+@aiohttp.web.middleware
+async def middleware_debug(request, handler):
+    print(f'debug: {request=}')
+
+    if request.method == "CONNECT":
+        resp = aiohttp.web.StreamResponse()
+        resp._length_check = False
+        await resp.prepare(request)
+        resp.force_close()
+
+        loop = asyncio.get_running_loop()
+        on_con_lost = loop.create_future()
+        protocol = DummyHTTPResponse(on_con_lost)
+        protocol.connection_made(request.transport)
+        request.transport.set_protocol(protocol)
+        await on_con_lost
+        return
+
+    return await handler(request)
+
+class DummyHTTPResponse(asyncio.Protocol):
+    def __init__(self, on_con_lost):
+        self.on_con_lost = on_con_lost
+
+    def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
+        self.transport = transport
+
+    def data_received(self, data):
+        message = data.decode()
+        print('Data received: {!r}'.format(message))
+
+        body = "Hello, World!\n"
+        response = "\r\n".join([
+            "HTTP/1.1 200 Success",
+            "Content-Type: text/plain",
+            f"Content-Length: {len(body)}",
+            "Server: Fake CONNECT",
+            "",
+            body
+        ])
+        self.transport.write(response.encode())
+
+        print('Close the client socket')
+        self.transport.close()
+
+    def connection_lost(self, exc):
+        print('The client closed the connection')
+        self.on_con_lost.set_result(True)
+
+
+def parse_config():
+    # TODO: parse real config file
+    return {
+        "places": {
+            "0": {
+                "boards": {
+                    "example-board": {
+                        "interfaces": {
+                            "usb0": {
+                                "type": "usb",
+                            }
+                        },
+                    },
+                },
+            },
+        }
+    }
 
 
 def json_get(path):
     def decorator(f):
-        @ROUTES.get(path)
+        @routes.get(path)
         @functools.wraps(f)
         async def wrapper(*args, **kwargs):
             response = await f(*args, **kwargs)
@@ -45,12 +118,16 @@ def json_get(path):
 
 @json_get('/api/v1/places')
 async def handle_places(request):
-    return { "message": "Hello, World!" }
+    config = request.app["config"]
+    return { "places": config.get("places", {}) }
 
 # /api/v1/places
-# /api/v1/places/{id}
-# /api/v1/places/{id}/shared/wifi
-# /api/v1/places/{id}/boards/{board}
-# /api/v1/places/{id}/boards/{board}/usb0
-# /api/v1/places/{id}/boards/{board}/usb-serial
-# /api/v1/places/{id}/boards/{board}/control
+# /api/v1/places/{place}
+# /api/v1/places/{place}/shared/wifi
+# /api/v1/places/{place}/boards/{board}
+# /api/v1/places/{place}/boards/{board}/interfaces
+# /api/v1/places/{place}/boards/{board}/interfaces/{interface}
+
+# /api/v1/places/{place}/boards/{board}/usb0
+# /api/v1/places/{place}/boards/{board}/usb-serial
+# /api/v1/places/{place}/boards/{board}/control
