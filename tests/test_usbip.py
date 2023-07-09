8e6e721f7533ff88fdf23abf9e0bd054cc69862e
Simon Holesch <simon@holesch.de>
2023-07-09 12:32:34 +0200

    tests: Add basic usbip test case
    It doesn't test much, but it uses the vmctl script to setup the three
    VMs, attaches a virtual USB device and forwards it with USB/IP to
    another VM.

---
+++ tests/test_usbip.py
@@ -0,0 +1,176 @@
+import asyncio
+import collections
+import contextlib
+import pathlib
+import sys
+
+import pytest
+
+import not_my_board._util as util
+
+
+class _VM:
+    _name = ""
+
+    async def __aenter__(self):
+        async with contextlib.AsyncExitStack() as stack:
+            await stack.enter_async_context(
+                sh_task(f"./scripts/vmctl run {self._name}", f"vm {self._name}")
+            )
+
+            self._stack = stack.pop_all()
+            await self._stack.__aenter__()
+            return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self._stack.__aexit__(exc_type, exc, tb)
+
+    async def configure(self):
+        await sh(
+            f"./scripts/vmctl configure {self._name}", prefix=f"configure {self._name}"
+        )
+
+    def ssh_task(self, cmd, *args, **kwargs):
+        return sh_task(f"./scripts/vmctl ssh {self._name} " + cmd, *args, **kwargs)
+
+    async def ssh(self, cmd, *args, **kwargs):
+        return await sh(f"./scripts/vmctl ssh {self._name} " + cmd, *args, **kwargs)
+
+    async def ssh_poll(self, cmd, timeout=7):
+        return await sh_poll(f"./scripts/vmctl ssh {self._name} " + cmd, timeout)
+
+
+class ServerVM(_VM):
+    _name = "server"
+    ip = "192.168.200.1"
+
+
+class ExporterVM(_VM):
+    _name = "exporter"
+    ip = "192.168.200.2"
+
+    async def usb_attach(self):
+        await sh("./scripts/vmctl usb attach")
+
+    async def usb_detach(self):
+        await sh("./scripts/vmctl usb detach")
+
+
+class ClientVM(_VM):
+    _name = "client"
+    ip = "192.168.200.3"
+
+
+VMs = collections.namedtuple("VMs", ["server", "exporter", "client"])
+
+
+@pytest.fixture
+async def vms():
+    async with ServerVM() as server:
+        while True:
+            try:
+                async with util.connect("127.0.0.1", 5001):
+                    pass
+                async with util.connect("127.0.0.1", 5002):
+                    pass
+            except ConnectionRefusedError:
+                await asyncio.sleep(0.1)
+                continue
+            break
+        async with ExporterVM() as exporter:
+            async with ClientVM() as client:
+                await util.run_concurrently(
+                    server.configure(),
+                    exporter.configure(),
+                    client.configure(),
+                )
+                yield VMs(server, exporter, client)
+
+
+async def test_usb_forwarding(vms):
+    await vms.exporter.usb_attach()
+    async with vms.exporter.ssh_task(
+        "doas python3 -m not_my_board._usbip export 2-1", "usbip export"
+    ):
+        # wait for listening socket
+        await vms.exporter.ssh_poll("nc -z 127.0.0.1 3240")
+
+        async with vms.client.ssh_task(
+            f"doas python3 -m not_my_board._usbip import {vms.exporter.ip} 2-1 0",
+            "usbip import",
+        ):
+            # wait for USB device to appear
+            await vms.client.ssh_poll("test -e /dev/usbdisk")
+
+            await vms.client.ssh("doas mount /media/usb")
+            try:
+                result = await vms.client.ssh("cat /media/usb/hello")
+                assert result.stdout == "Hello, World!"
+            finally:
+                await vms.client.ssh("doas umount /media/usb")
+
+
+ShResult = collections.namedtuple("ShResult", ["stdout", "stderr", "returncode"])
+
+
+async def sh(cmd, check=True, strip=True, prefix=None):
+    proc = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+
+    stdout, _ = await util.run_concurrently(
+        proc.stdout.read(), _log_output(proc.stderr, cmd, prefix)
+    )
+    await proc.wait()
+    if check and proc.returncode:
+        raise RuntimeError("{cmd!r} exited with {proc.returncode}")
+
+    stdout = stdout.decode("utf-8")
+    if strip:
+        stdout = stdout.rstrip()
+
+    return ShResult(stdout, None, proc.returncode)
+
+
+@contextlib.asynccontextmanager
+async def sh_task(cmd, prefix=None):
+    # need to exec, otherwise only the shell process is killed with
+    # proc.terminate()
+    proc = await asyncio.create_subprocess_shell(
+        f"exec {cmd}",
+        stdin=asyncio.subprocess.DEVNULL,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.STDOUT,
+    )
+
+    logging_task = None
+    try:
+        logging_task = asyncio.create_task(_log_output(proc.stdout, cmd, prefix))
+        yield
+    finally:
+        proc.terminate()
+        await proc.wait()
+        if logging_task:
+            await logging_task
+
+
+async def sh_poll(cmd, timeout=7):
+    async def poll_loop():
+        while True:
+            result = await sh(cmd, check=False)
+            if result.returncode == 0:
+                break
+            await asyncio.sleep(0.1)
+
+    await asyncio.wait_for(poll_loop(), timeout)
+
+
+async def _log_output(stream, cmd, prefix):
+    if prefix is None:
+        prefix = f"[{cmd}] ".encode()
+    else:
+        prefix = f"[{prefix}] ".encode()
+
+    async for line in stream:
+        sys.stderr.buffer.write(prefix + line)
+        sys.stderr.buffer.flush()
